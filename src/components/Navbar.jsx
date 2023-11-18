b89acb839cf6e388edd4dacc4aa826c4d4ea843a
ankitjhagithub21 <ajha45075@gmail.com>
2023-11-18 11:41:40 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 
 import links from '../api/NavbarApi';
+
 const Navbar = () => {
   const [isFixed, setIsFixed] = useState(false);
+  const [menuClass, setMenuClass] = useState('bars');
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
     let timeoutId;
@@ -29,30 +31,38 @@ const Navbar = () => {
     };
   }, [isFixed]);
 
-  const navbarClassName = `navbar${isFixed ? ' fixed' : ''}`;
+  const navbarClassName = `navbar${isFixed ? ' fixed' : ''}${active ? ' active' : ''}`;
+
+  const toggleMenu = () => {
+    if (menuClass === 'bars') {
+      setMenuClass('xmark');
+      setActive(true);
+    } else {
+      setMenuClass('bars');
+      setActive(false);
+    }
+  };
 
   return (
-    
-    <header className={navbarClassName} >
-  <div className="container mx-auto flex flex-wrap py-6 flex-col md:flex-row items-center">
-   
-      
-      <h1 className="ml-3 text-4xl font-bold"><span className='text-red-500'>CAR</span> DEALER</h1>
-  
-    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center md:mt-2">
-      
-     {
-      links.map((link)=>{
-        return(
-          <Link className="mr-5 lg:text-lg" key={link.path} to={link.path}>{link.name}</Link>
-        )
-      })
-     }
-    </nav>
-   
-  </div>
-</header>
-  )
-}
+    <header className={navbarClassName}>
+      <div className="container mx-auto flex flex-wrap py-6 flex-col md:flex-row items-center">
+        <h1 className="ml-3 text-4xl font-bold" id="logo">
+          <span className="text-red-500">CAR</span> DEALER
+        </h1>
+        <nav
+          className={`md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center md:mt-2 ${active ? 'active' : ''}`}
+          id="nav"
+        >
+          {links.map((link) => (
+            <Link className="mr-5 lg:text-lg" key={link.path} to={link.path}>
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+        <i className={`fa-solid fa-${menuClass} absolute top-3 text-xl right-3`} onClick={toggleMenu} id='menuIcon'></i>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
